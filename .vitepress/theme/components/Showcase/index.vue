80f5c0f64d45083e445d0f8fdf24840d86e45a25
naahi-i <69071025+naahi-i@users.noreply.github.com>
2024-11-02 19:04:36 +0800

    Update index.vue

--- .vitepress/theme/components/Showcase/index.vue
+++ .vitepress/theme/components/Showcase/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <Showcase
-            imageSrc="../img/win11-tan.jpg"
+            :imageSrc="img1"
             title="Windows11-tan"
             subtitle1=""
             subtitle2="win11娘可爱捏"
@@ -9,17 +9,18 @@
             imgHeight="100%"
         />
         <Showcase
-            imageSrc="../img/pln.jpg"
+            :imageSrc="img2"
             title="普拉娜"
             subtitle1=""
             subtitle2="橱窗正在施工"
             :isReversed="true"
             imgHeight="140%"
-
         />
     </div>
 </template>
 
 <script setup lang="ts">
 import Showcase from './Showcase.vue'; // 导入通用橱窗组件
+import img1 from '../img/win11-tan.jpg'; // 引入图片
+import img2 from '../img/pln.jpg'; 
 </script>
