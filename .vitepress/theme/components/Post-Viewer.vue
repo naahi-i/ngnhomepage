cbae2e276132a425337b3bdf91ab956766317f0a
naahi-i <69071025+naahi-i@users.noreply.github.com>
2024-11-22 23:54:10 +0800

    合并上游更新

--- .vitepress/theme/components/Post-Viewer.vue
+++ .vitepress/theme/components/Post-Viewer.vue
@@ -349,12 +349,14 @@ const themeConfig = useData().theme.value
       overflow-y: hidden;
     }
 
+    .vp-code {
+      font-family: monospace;
+    }
+
     .line-numbers-wrapper {
       border-right: 2px solid #dfdfdf;
-      // padding: 0 8px;
-      margin-right: 5px;
+      text-align: center;
       user-select: none;
-      font-weight: lighter;
     }
 
     pre,
