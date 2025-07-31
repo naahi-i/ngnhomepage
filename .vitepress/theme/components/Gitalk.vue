df4c5f3152094802a845ec1a1835224027de8754
naahi-i <2228055359@qq.com>
2025-07-31 18:33:57 +0800

    调整gitalk评论区字体颜色以便于阅读
    调整gitalk评论区字体颜色以便于阅读

--- .vitepress/theme/components/Gitalk.vue
+++ .vitepress/theme/components/Gitalk.vue
@@ -31,5 +31,36 @@ onMounted(() => {
   color: var(--font-color-grey);
   background-color: var(--general-background-color) !important;
   transition: background-color 0.5s, color 0.5s !important;
+  
+}
+.gt-container .gt-comment-content{
+  background-color: var(--gitalk-background) !important;
+  border-radius: 10px;
+  p{
+    color: var(--font-color-grey);
+  }
+  ol{
+    color: var(--gitalk-font-color-ol);
+  }
+  .email-fragment {
+    color: var(--font-color-grey);
+  }
+  .email-hidden-reply {
+    color: var(--font-color-grey);
+  }
+}
+
+.gt-container .gt-comment-content:hover {
+  -webkit-box-shadow: var(--gitalk-shadow) !important;
+          box-shadow: var(--gitalk-shadow) !important; 
+}
+
+.gt-container .gt-comment-body {
+  color: #ffffff !important;
+}
+
+.markdown-body blockquote {
+  padding: 0 1em;
+  border-left: var(--gitalk-border-left) !important;
 }
 </style>
